164dc4e598c9d9c34424378099c1df3deb5e3adf
Leilayne Silva <leilayness@gmail.com>
2024-09-11 19:37:22 -0300

    tabuada

---
+++ dia7/dia7.py
@@ -0,0 +1,9 @@
+def gerar_tabuada(numero):
+  print(f"Tabuada do {numero}")
+  for i in range(1, 11):
+    resultado = numero * i 
+    print(f"{numero} x {i} = {resultado}")
+  
+numero = int(input("Digite um número para gerar a tabuada: "))
+
+gerar_tabuada(numero)
